0465d19b0e33311939275e33788cf6a3122fccf6
BeastBoy <timur22090505@gmail.com>
2024-01-23 21:16:15 +0600

    +HomeWork1

---
+++ Lab1/Comments.py
@@ -0,0 +1,9 @@
+#Exercise1
+#This is a comment
+
+#Exercise2
+"""
+This is a comment
+written in 
+more than just one line
+"""
---
+++ Lab1/Data Types.py
@@ -0,0 +1,34 @@
+#Exercise1
+x=5
+print(type(x))
+#Output:int
+
+#Exercise2
+x = "Hello World"
+print(type(x))
+#Output:str
+
+#Exercise3
+x = 20.5
+print(type(x))
+#Output: float
+
+#Exercise4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Output: list
+
+#Exercise5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Output:tuple
+
+#Exercise6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Output:dict
+
+#Exercise7
+x = True
+print(type(x))
+#Output:bool
